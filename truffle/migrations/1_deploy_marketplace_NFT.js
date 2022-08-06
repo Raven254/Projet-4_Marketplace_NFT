576ccfd38cefd4eccb7ea3a9bbbc48f4dc19e237
0xMeurwo <marwaane.eljaafari@gmail.com>
2022-08-07 01:47:00 +0200

    ajout addresse C

--- truffle/migrations/1_deploy_marketplace_NFT.js
+++ truffle/migrations/1_deploy_marketplace_NFT.js
@@ -3,6 +3,6 @@ const MarketplaceNFT = artifacts.require("MarketplaceNFT");
 
 module.exports = function (deployer) {
   deployer.deploy(NFTFactory).then(function(){
-    return deployer.deploy(MarketplaceNFT, 25, "0xFF0B5Aa862bDb101be86cE3C10440CAe866fFcEe", "0x7F5aF8d07770d2Df562409cD447D90E670A3eedb", "0xFF0B5Aa862bDb101be86cE3C10440CAe866fFcEe");
+    return deployer.deploy(MarketplaceNFT, 25, "0x4421170B2A7Eb9826AAfc9788Af8865D5b48CE16", "0x7F5aF8d07770d2Df562409cD447D90E670A3eedb", "0xFF0B5Aa862bDb101be86cE3C10440CAe866fFcEe");
   });
 };
